b43da308ce34676a726f3b72e6ead15f507573fe
Michael O'Brien <>
2023-03-12 00:51:32 -0500

    updates

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # rdns
 
-
+# IDEAS
+ - The goal is to create a distrubuted systems that can dynimcally scale number of works based
+ IPV4 spaces, desired time to have tasking completed, queries per second
+ - Create service to discover public resolvers executed recursive queries. This ips of ther resolves will then be used by the workers for executing DNS queries 
+ - 
 
 # Running
 ```bash
@@ -18,3 +22,22 @@ docker run -ti --volume $RDNS_HOME/rdns.py:/root/rdns.py rdns bash
 
 # example run
 python3 rdns.py run --cidr 128.8.0.0/24 --destination 1.1.1.1 --qps 500
+
+```
+
+
+# Current stats
+```
+root@8a50181e61ca:~# python3 rdns.py run --cidr 128.8.0.0/24 --destination 1.1.1.1 --qps 500
+#names/#queries: 66 / 256
+sent DNS querys for 44.38332557678223s
+writing results to /data/rdns.json
+ Elapse time writing results to disk: 0.0008902549743652344s
+wrote files to disk
+runtime: 44.38510274887085
+```
+
+
+# Test 1
+	python3 rdns.py run --cidr 128.8.0.0/16 --destination 1.1.1.1 --qps 500
+	python3 rdns.py run --cidr 46.101.0.0/16 --destination 8.8.8.8 --qps 500
--- rdns.py
+++ rdns.py
@@ -1,8 +1,11 @@
 import ipaddress
 import time
 import argparse
+import json
 from scapy.all import *
 
+
+# Todo: create a logger that wraps python logging
 
 
 def expand_cidr(cidr):
@@ -16,6 +19,19 @@ def expand_cidr(cidr):
 
 
 def rdns(cidr='128.8.0.0/24', dns_server_ip='8.8.8.8', qps=None):
+	"""
+	Executes a reverse dns look up on input cidr ips using specified dns server
+	The reverse DNS queries can be rate limited using the qps param
+
+	Args:
+		cidr (str): IPV4 network cidr
+		dns_server_ip (str): IP of dns server dns query will be executed against
+		qps (int) : Desired number of DNS queryies executed per minute
+
+	Returns:
+		dict: Composed of key/value pairs s.t. Key=IPV4 address and value is the host name of the ipv4 address
+	"""
+
 	# Todo: factor in dns lookup time in to wait time
 	# TODO: calculate actually rate dns packets are being sent/recv
 	# Assums dns lookup is constant time 
@@ -23,46 +39,78 @@ def rdns(cidr='128.8.0.0/24', dns_server_ip='8.8.8.8', qps=None):
 	if qps:
 		wait = 60 / qps
 
-	nets = expand_cidr(cidr)
-	results = []
+	total_name_chars = 0
 
+	nets = expand_cidr(cidr)
+	results = dict()
+	st = time.time()
 	for net in nets:
 		# TODO: dynamically throttle request rate by qps parameter
-
 		qname = ipaddress.ip_address(net).reverse_pointer  # convert ip address to reverse pointer
 		ans = sr1(IP(dst=dns_server_ip)/UDP()/DNS(rd=1,qd=DNSQR(qname=qname, qtype='PTR')))
-		# breakpoint()
 
 		if ans.an:
-			name = ans.an.rdata
-			results.append((net, name))
+			name = ans.an.rdata.decode("utf-8") 
+			results[net] = name
+
+			total_name_chars += len(name)
+			# print (f'name: {type(name)}, {name}')
+			# print (f'net: {type(net)}, {net}')
 		else:
 			name = None
 
 		if qps:
 			time.sleep(wait)
 
-		# print (f'{ans!r}')
-		# print (f'######### {net} - {name} ##########')
 
-		# if net == '128.8.0.2':
-			# break
-
-	print (f'{results}')
+	# print (f'{results}')
+	print (f'avg name length: {total_name_chars / len(results)}')
 	print (f'#names/#queries: {len(results)} / {len(nets)}')
+	print(f'sent DNS querys for {time.time() - st}s')
 
+	path = '/data/rdns.json'
+	print (f'writing results to {path}')
+	store_dns(results=results, path=path)
+	print (f'wrote files to disk')
+
+def store_dns(results, path='/data/rdns.json'):
+	"""
+	Convert result data to json and write to disk
+
+	Args:
+	    results (dict): Key=IPV4 address and value is the host name of the ipv4 address
+
+	Returns:
+		bool: hard coded True value
+
+	"""
+	st = time.time()
+	results_json = json.dumps(results)
+
+	if not os.path.exists('/data'):
+		os.mkdir('/data')
+
+	with open('/data/rdns.json', 'w') as f:
+		f.write(results_json)
+
+	print (f' Elapse time writing results to disk: {time.time() - st}s')
+
+	return True
+
+
+
+# ------------------------------------------------------------------------
 def cli_rdns(args):
 	"""
-
+	Invoed by running rdns from the commandline
 	"""
-
 	start = time.time()
 	conf.verb = 0 # disable scapy debug statements
 	rdns(cidr=args.cidr, dns_server_ip=args.destination, qps=args.qps)
 
 	print (f'runtime: {time.time() - start}')
 
-
+# ------------------------------------------------------------------------
 
 if __name__ == "__main__":
 
