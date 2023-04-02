47e7ebbf0216f3f5376f99b550450c6fdb73169a
Michael O'Brien <>
2023-04-01 23:45:00 -0400

    add workflow to process results and display aggregated stats

--- godns/dns.go
+++ godns/dns.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     "net"
+    "errors"
     "os"
     "log"
     "strconv"
@@ -21,7 +22,14 @@ type Task struct {
     qps int
 }
 
-func rdns(lookupIP string, dnsServer string) (string) {
+type Slash24Result struct {
+    resolver string
+    timeout_ips []string
+    noname_ips []string
+    ipToName map[string]string
+} 
+
+func rdns(lookupIP string, dnsServer string) (string, error) {
     /*
 
     Perform reverse DNS lookup on lookupIP and execute the query against dnsServer 
@@ -50,6 +58,7 @@ func rdns(lookupIP string, dnsServer string) (string) {
     rev, err := dns.ReverseAddr(ip.String())
     if err != nil {
         fmt.Println(err)
+        return lookupIP, err
     }
 
     // TODO: add logic to store ips when the cnx times out
@@ -57,11 +66,12 @@ func rdns(lookupIP string, dnsServer string) (string) {
     msg.SetQuestion(rev, dns.TypePTR)
     resp, _, err := c.Exchange(&msg, dnsServer+":53")
     if err != nil {
-        fmt.Println("ERROR", err, "ip: ",ip, "resolver: ", dnsServer)
+        // fmt.Println("ERROR", err, "ip: ",ip, "resolver: ", dnsServer)
+        return "timeout", errors.New("query timeout")
     }
     if resp == nil {
         // fmt.Println(ip,"-", dnsServer,"failed PTR look up")
-        return ""
+        return "a", nil
     }
 
     // TODO: condense code to single return statement
@@ -71,21 +81,23 @@ func rdns(lookupIP string, dnsServer string) (string) {
             if ptr, ok := ans.(*dns.PTR); ok {
                 hostname := ptr.Ptr
                 // fmt.Println(dnsServer, ip, hostname)
-                return hostname
+                return hostname, nil
 
 
             } else {
-                return ""
+                return "b", nil
             }
         }
     } else {
-        return ""
+        // fmt.Println(lookupIP, "noname")
+        return "noname", errors.New("query timeout")
     }
-    return ""
+
+    return "c", nil
 
 }
 
-func lookUpSlash24(prefix string, dnsServer string) (map[string]string) {
+func lookUpSlash24(prefix string, dnsServer string) (Slash24Result) {
     /*
     The function will query th entire /24 of the prefix privded
     The entire lookup will take ~8s against 1.1.1.1 or 8.8.8.8
@@ -95,24 +107,49 @@ func lookUpSlash24(prefix string, dnsServer string) (map[string]string) {
     Returns:
         Nothing rn  
     */
+    var noname_ips []string
+    var timeout_ips []string
+
     var ipToName = make(map[string]string)
     for i := 0; i <= 255; i++ {
-    // for i := 0; i <= 255; i++ {
         ip := prefix + strconv.Itoa(i)
         // fmt.Println(ip)
-        res := rdns(ip,dnsServer)
-        if len(res) > 0 {
+        res, err := rdns(ip,dnsServer)
+        if err != nil {
+            if res == "noname" {
+                noname_ips = append(noname_ips, ip)
+
+            }
+            if res == "timeout" {
+                timeout_ips = append(timeout_ips, ip)
+                // fmt.Println("timeout ", ip, dnsServer)
+
+            }
+        } else {
             ipToName[ip] = res
         }
+        // fmt.Println(res)
 
 
         // fmt.Println(ip,res,"res")
 
     }
 
-    fmt.Println(ipToName)
+
+    // ipToName["resolver"] = dnsServer
+    results := Slash24Result{dnsServer, timeout_ips, noname_ips, ipToName}
+    // fmt.Println("results",results)
+    // fmt.Println("names", len(ipToName))
+    // fmt.Println("noname_ips", len(noname_ips))
+    // fmt.Println("timeout_ips", len(timeout_ips))
+    // ipToName["noname"] = noname_ips
+    // ipToName["timeout"] = timeout_ips
+
+
+
+    // fmt.Println(ipToName)
     // fmt.Println(ipToName["128.8.0.1"])
-    return ipToName
+    return results
 }
 
 func recv_tasking(path string) []Task {
@@ -155,25 +192,54 @@ func recv_tasking(path string) []Task {
 
     return tasks
 }
+func process_results(c chan Slash24Result) {
+// func process_results(c chan Slash24Result, wg sync.WaitGroup) {
+    // defer wg.Done()
+    fmt.Println("start")
+    names := 0
+    nonames := 0
+    timeouts := 0
+    for res := range c {
+        timeouts += len(res.timeout_ips)
+        nonames += len(res.noname_ips)
+        names += len(res.ipToName)
+        // fmt.Println("task result:", res)
+    }
+    fmt.Println("names:", names)
+    fmt.Println("nonames:", nonames)
+    fmt.Println("timeouts:", timeouts)
+    fmt.Println("names/ip: ", names, "/", names+nonames+timeouts)
+    fmt.Println("end")
+}
+
+
+
+
 
 func main() {
     /*
 
     */
     tasking := recv_tasking("/root/tasking.json")
-    fmt.Println(tasking)
+    fmt.Println("tasking", tasking)
     var wg sync.WaitGroup
+    c := make(chan Slash24Result)
 
     i := 0
     for _, task := range tasking {
-        // fmt.Println(task)
+        fmt.Println("task", task)
         wg.Add(1)
 
-        go func(task Task) {
+        go func(task Task, c chan Slash24Result) {
             defer wg.Done()
             // fmt.Println(task)
-            lookUpSlash24(task.cidr, task.resolver)
-        }(task)
+            result := lookUpSlash24(task.cidr, task.resolver)
+            // go process_results(result)
+            c <- result
+            // fmt.Println(result)
+        }(task, c)
+
+        // fmt.Println(res)
 
         if i % 10 == 0 {
             time.Sleep(5 * time.Second)
@@ -181,7 +247,12 @@ func main() {
 
         i++
     }
-    wg.Wait()
+    go func(c chan Slash24Result) {
+        defer close(c)
+        wg.Wait()
+    }(c)
+
+    process_results(c)
 
     // TODO: parse cnx details from input params
     // TODO: Connect to master node and listen for tasking
